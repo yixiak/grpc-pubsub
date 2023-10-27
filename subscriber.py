b6fa45d72b001e70ab6bb855571d623356cc5326
Yixa <1257597350@qq.com>
2023-10-27 17:43:21 +0800

    feat: subscribe for client

--- subscriber.py
+++ subscriber.py
@@ -10,3 +10,15 @@ import subprocess
 import grpc
 import pubsub_pb2
 import pubsub_pb2_grpc
+
+def subscribe(theme_index):
+    channel = grpc.insecure_channel('localhost:50051')
+    client = pubsub_pb2_grpc.pubsubStub(channel)
+    
+    request = pubsub_pb2.theme(theme_index=theme_index)
+    response = client.subscribe(request)
+    
+    if response and response.theme_index==theme_index:
+        print(f"Subscribe to {theme_index} successfully")
+    else:
+        print(f"Failed to subscribe to {theme_index}")
